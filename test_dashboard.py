8f223b0446e3094f7b4291e1a1173edbf0dc9003
Shiv Shah <shahshiv@gmail.com>
2026-04-29 22:15:19 -0400

    Added placeholder for dashboard testing

---
+++ test_dashboard.py
@@ -0,0 +1,15 @@
+import unittest
+import io
+
+from unittest.mock import patch
+
+from model import Issue
+
+class TestDashboard(unittest.TestCase):
+    
+    # Placeholder for patching in functions
+    def test_dashboard_simple(self):
+        self.assertEqual(True, True)
+
+if __name__ == "__main__":
+    unittest.main()
