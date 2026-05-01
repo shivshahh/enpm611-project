a096b128b1a1f40501c3ac59dc79a9b66b0a6656
Michael Hunt <travler7282@comcast.net>
2026-04-30 21:38:28 -0400

    Added test case += 1 paths

--- test_labels.py
+++ test_labels.py
@@ -32,7 +32,41 @@ class TestLabels(unittest.TestCase):
     
         self.assertIn("bug average time to close:", output)
 
-    # MORE TESTS HERE
+    # Duplicate label increment (+=1 paths)
+        @patch('features.labels_analysis.plt.show') # stop the graph from showing up
+        @patch('features.labels_analysis.DataLoader') # control what data gets loaded
+        @patch('features.labels_analysis.config.get_parameter', return_value=None) # simulate no label arg passed
+        @patch('sys.stdout', new_callable=io.StringIO) # get stdout
+        def test_duplicate_label_increments(self, mock_stdout, mock_config, mock_loader, mock_show):
+            # Mock issue to confirm the output
+            mock_issues = [
+                Issue({
+                    "labels": ["bug", "bug"],
+                    "state": "closed",
+                    "created_date": "2026-01-23T08:12:56Z",
+                    "updated_date": "2026-04-28T09:34:56Z"
+                }),
+                Issue({
+                    "labels": ["bug"],
+                    "state": "closed",
+                    "created_date": "2025-02-15T10:00:00Z",
+                    "updated_date": "2025-12-28T11:00:00Z"
+                })
+            
+            ]
+
+            # mock loading the issue
+            mock_loader.return_value.get_issues.return_value = [mock_issues[0], mock_issues[1]]
+
+            # run label analyis
+            analysis = LabelsAnalysis()
+            analysis.run()
+        
+            # ensure the label_count for bug is 2 and not something else
+            self.assertEqual(analysis.label_count["bug"], 2)
+
+            # ensure the "bug avaerage time to close:" is in the stdout output
+            self.assertIn("bug average time to close:", mock_stdout.getvalue())
 
 if __name__ == "__main__":
     unittest.main()
